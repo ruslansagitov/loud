7a695620035cbbf132ba66d583fc42015b376f24
Ruslan Sagitov <rs@ruslansagitov.name>
2014-07-21 19:14:09 +0400

    Connect to SauceLabs

--- karma.conf.js
+++ karma.conf.js
@@ -4,4 +4,27 @@ module.exports = function(config) {
         browsers: ['Firefox', 'PhantomJS'],
         files: ['build/test.js']
     });
+
+    if (process.env.CI) {
+        var customLaunchers = {
+            'SauceLabs_Firefox': {
+                base: 'SauceLabs',
+                browserName: 'firefox'
+            },
+            'SauceLabs_Chrome': {
+                base: 'SauceLabs',
+                browserName: 'chrome'
+            }
+        };
+
+        config.set({
+            browsers: Object.keys(customLaunchers),
+            reporters: ['progress', 'saucelabs'],
+            captureTimeout: 120000,
+            sauceLabs: {
+                testName: 'Loud'
+            },
+            customLaunchers: customLaunchers
+        });
+    }
 };
