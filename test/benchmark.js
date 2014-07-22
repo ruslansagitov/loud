a89aa4e334cc3a1651f7a97a6b86698b764400a8
Ruslan Sagitov <rs@ruslansagitov.name>
2014-07-22 19:19:53 +0400

    Test performance

---
+++ test/benchmark.js
@@ -0,0 +1,28 @@
+var Util = require('util'),
+    Benchmark = require('benchmark'),
+    Loud = require('../lib/loud');
+
+var suite = new Benchmark.Suite(),
+    loud = new Loud();
+
+var data = [
+    'Text',
+    '<h1 role="presentation">Content</h1>',
+    '<button>Button</button>',
+    '<input type="range" min="0" value="1" max="4">',
+    '<button aria-labelledby="label"></button><span id="label">Button</span>',
+    '<ul><li>One <input value="Two"> Three</li></ul>',
+    '<table><tbody><tr><td>Content</td></tr></tbody></table>'
+];
+
+data.forEach(function(item) {
+    suite.add(item, function() {
+        loud.say(item);
+    });
+});
+
+suite.on('cycle', function(e) {
+    Util.puts(e.target.toString());
+}).run({
+    async: true
+});
