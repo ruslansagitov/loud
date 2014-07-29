fb98dc35bd394c5bc170857de5df5bae79b434a6
Ruslan Sagitov <rs@ruslansagitov.name>
2014-07-29 20:28:51 +0400

    Stop testing in SauceLabs

--- karma.conf.js
+++ karma.conf.js
@@ -5,27 +5,4 @@ module.exports = function(config) {
         files: [require.resolve('es5-shim'), 'build/test.js'],
         reporters: ['dots']
     });
-
-    if (process.env.CI && process.env.SAUCE_ACCESS_KEY) {
-        var customLaunchers = {
-            sauceLabsFirefox: {
-                base: 'SauceLabs',
-                browserName: 'firefox'
-            },
-            sauceLabsChrome: {
-                base: 'SauceLabs',
-                browserName: 'chrome'
-            }
-        };
-
-        config.set({
-            browsers: Object.keys(customLaunchers),
-            reporters: ['dots', 'saucelabs'],
-            captureTimeout: 120000,
-            sauceLabs: {
-                testName: 'Loud'
-            },
-            customLaunchers: customLaunchers
-        });
-    }
 };
