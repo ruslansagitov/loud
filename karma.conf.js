0bfd4ac771d9ea01cf51313d1c1fa6a07784ccb2
Ruslan Sagitov <rs@ruslansagitov.name>
2015-03-04 17:23:46 +0300

    Run tests in SauceLabs
    Test in Internet Explorer.

--- karma.conf.js
+++ karma.conf.js
@@ -11,4 +11,28 @@ module.exports = function(config) {
         files: ['build/test.js'],
         reporters: ['dots']
     });
+
+    if (process.env.CI && process.env.SAUCE_ACCESS_KEY) {
+        var customLaunchers = {
+            sauceLabsIE11: {
+                base: 'SauceLabs',
+                browserName: 'internet explorer',
+                version: 11
+            },
+            sauceLabsChrome: {
+                base: 'SauceLabs',
+                browserName: 'chrome'
+            }
+        };
+
+        config.set({
+            browsers: browsers.concat(Object.keys(customLaunchers)),
+            reporters: ['dots', 'saucelabs'],
+            captureTimeout: 120000,
+            sauceLabs: {
+                testName: 'Loud'
+            },
+            customLaunchers: customLaunchers
+        });
+    }
 };
