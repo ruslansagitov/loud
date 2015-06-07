6c87a6de77ace60f5992a8800a1e0c0c012d5ba7
Ruslan Sagitov <rs@ruslansagitov.name>
2015-06-07 18:05:22 +0300

    Drop the wildcard expression to work on Windows
    Return back a single entry point for tests.

---
+++ test/test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+require('./test-accessible-name');
+require('./test-role-from-tag');
+require('./test-words-from-role');
+require('./test-words-from-attributes');
+require('./test-misc');
