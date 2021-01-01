0f634cf67db45c0567981eb2b33b2a590cf44975
Ruslan Sagitov <rs@ruslansagitov.name>
2021-01-01 14:28:04 +0300

    Downgrade testing in Android

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function(config) {
                 base: 'SauceLabs',
                 browserName: 'Chrome',
                 deviceName: 'Android GoogleAPI Emulator',
-                platformVersion: '10.0',
+                platformVersion: '8.1',
                 platformName: 'Android'
             }
         };
